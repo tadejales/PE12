1fc7759f1ac858bc0267a894e573825721d3d8ee
tadejales <tadejales@gmail.com>
2016-10-04 19:51:13 +0200

    Posodobitev

--- PE12.py
+++ PE12.py
@@ -7,3 +7,23 @@ def dividors(number): #deljitelji števila
                 return devidors_list
 
     return False
+
+    curent_number_list = []
+    curent_number = 0
+    sum_of_curent_numbers = 0
+
+    """
+    while 1:
+        curent_number_list.append(curent_number) #dodajanje trenutnega števila na listo
+        print (curent_number_list)
+        print (devidors_list)
+        if dividors(sum_of_curent_numbers) == True:
+            print (curent_number)
+            break
+        else:
+            break
+        curent_number += 1
+        sum_of_curent_numbers = sum_of_curent_numbers + curent_number
+    """
+
+    print(dividors(21))
